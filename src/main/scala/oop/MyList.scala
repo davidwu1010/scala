ac4c5a6342e03ca072532dd0d800abf7d59610fa
David Wu <wudawei01@gmail.com>
2020-12-19 22:33:23 +0000

    3

---
+++ src/main/scala/oop/MyList.scala
@@ -0,0 +1,32 @@
+package oop
+
+abstract class MyList {
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
+}
+
+object Empty extends MyList {
+  override def head: Int = throw new NoSuchElementException
+  override def tail: MyList = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+  override def add(element: Int): MyList = new Cons(element, Empty)
+  override def printElements: String = ""
+}
+
+class Cons(h: Int, t: MyList) extends MyList {
+  override def head: Int = h
+  override def tail: MyList = t
+  override def isEmpty: Boolean = false
+  override def add(element: Int): MyList = new Cons(element, this)
+  override def printElements: String = {
+    if (t.isEmpty) {
+      h.toString
+    } else {
+      h + ", " + t.printElements
+    }
+  }
+}
